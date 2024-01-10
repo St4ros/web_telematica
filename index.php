c069b553f19bc5641b57eb62e32a6c5062afcb14
St4ros <dstevenark@gmail.com>
2024-01-10 13:23:13 -0500

    Tu mensaje de commit aquí

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "1wenas.321";
+$dbname = "mi_basededatos";
+
+// Conexión a la base de datos
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexion fallida: " . $conn->connect_error);
+}
+
+// Agregar nombre a la base de datos
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $sql = "INSERT INTO usuarios (nombre) VALUES ('$nombre')";
+    $conn->query($sql);
+}
+
+// Mostrar contenido de la base de datos
+$sql = "SELECT * FROM usuarios";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<ul>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<li>" . $row["nombre"] . "</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "La base de datos esta vacia.";
+}
+
+// Cerrar la conexión
+$conn->close();
+?>
